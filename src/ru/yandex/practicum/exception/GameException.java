8cf8196ed093116e3e0665d3da4eb425a6181e14
User Yana <ruavky4@yandex.ru>
2025-12-25 17:48:47 +0300

    Выолнененное задание в соотвествии с ТЗ спринта

--- src/ru/yandex/practicum/exception/GameException.java
+++ src/ru/yandex/practicum/exception/GameException.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.exception;
 
 public class GameException extends RuntimeException {
+
     public GameException(String message) {
         super(message);
     }
+
     public GameException(String message, Throwable cause) {
         super(message, cause);
     }
